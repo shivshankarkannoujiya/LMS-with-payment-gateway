06cd6efe8485557c680de67d83c0ebf2ccf2bef4
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-01-06 15:02:48 +0530

    add logging middleware and global error handler in app.js; update Prettier configuration

--- Server/src/app.js
+++ Server/src/app.js
@@ -2,12 +2,26 @@ import express from 'express';
 import morgan from 'morgan';
 const app = express();
 
+// logging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
 // Body Parser Middleware
-app.use(express.json({ limit: "10kb" }));
-app.use(express.urlencoded({extended: true, limit: "10kb"}))
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 
+// Global Error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(err.status || 500).json({
+        status: 'error',
+        message: err.message || 'Internal Server Error',
+        ...(process.env.NODE_ENV === 'development' && { stack: err.stack }),
+    });
+});
 
+// API Routes
 
 // it should be always at bottom
 // 404 handler
